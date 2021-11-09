477bd65128568572b9a901d9150c442801985663
sethclim <56656904+sethclim@users.noreply.github.com>
2021-11-09 14:56:59 -0500

    Click in Dots on Screen and Drag

---
+++ DraggableNodeIdentifiers.h
@@ -0,0 +1,22 @@
+/*
+  ==============================================================================
+
+    DraggableNodeIdentifiers.h
+    Created: 21 May 2021 1:14:56am
+    Author:  Seth Climenhaga
+
+  ==============================================================================
+*/
+
+#pragma once
+#include <JuceHeader.h>
+
+namespace DraggableNodeIdentifiers
+{
+    static const juce::Identifier myRootDraggableTreeType ("DraggableNodes");
+    static const juce::Identifier myNodeType ("DraggableNode");
+    static const juce::Identifier numberOfNodes ("numberOfNodes");
+    static const juce::Identifier posX ("posX");
+    static const juce::Identifier posY ("posY");
+
+}
